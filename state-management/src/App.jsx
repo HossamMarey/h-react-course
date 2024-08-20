08125e44a080a6109c0b07f78aee9ad116c066ab
HossamMarey <hmarey@ymail.com>
2024-08-20 18:52:18 +0300

    start

--- state-management/src/App.jsx
+++ state-management/src/App.jsx
@@ -1,16 +1,4 @@
-import { useEffect, useState } from "react";
-import useCounter from "./assets/hooks/useCounter";
-import MainCounter from "./assets/component/MainCounter";
-import {
-  CounterContext,
-  CounterProvider,
-} from "./assets/context/CounterContext";
-import HelloWorld from "./assets/component/HelloWorld";
-import { UsersProvider } from "./assets/context/UsersContext";
-import MainUsers from "./assets/component/MainUsers";
 import { createStore } from "redux";
-
-console.log("SSSSSSSSSS");
 
 // reducer
 const initialState = { counter: 0, ssss: "ccc" };
@@ -68,22 +56,7 @@ store.dispatch(() => {
 });
 
 function App() {
-  return (
-    <CounterProvider>
-      <UsersProvider>
-        <main className="min-h-screen py-24 ">
-          <div>
-            <MainUsers />
-          </div>
-          <div className="mb-12">
-            <MainCounter title="counter 1" />
-          </div>
-          <HelloWorld />
-          <MainCounter title="counter 2" />
-        </main>
-      </UsersProvider>
-    </CounterProvider>
-  );
+  return <div>APP</div>;
 }
 
 export default App;
--- state-management/src/assets/component/HelloWorld.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from "react";
-import { CounterContext, useCounterContext } from "../context/CounterContext";
-
-const HelloWorld = () => {
-  const { count } = useCounterContext();
-  return <div>HelloWorld {count}</div>;
-};
-
-export default HelloWorld;
